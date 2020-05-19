e59e0078ac333566cfbb68843efd711f7d5f358a
anhttmle <anhttmle@gmail.com>
2020-05-19 09:15:12 +0700

    Add Conv2LSTM_Maxout

--- models/core.py
+++ models/core.py
@@ -203,3 +203,61 @@ def build_lstm_maxout_dropout(hparams):
     )
 
     return model
+
+
+def build_conv_lstm_maxout_dropout(hparams):
+    keras.backend.clear_session()
+    rnn_input = layers.Input(shape=(15, 20))
+    mlp_input = layers.Input(shape=(30,))
+
+    rnn_imd = rnn_input
+    rnn_imd = layers.Conv1D(
+        filters=hparams["conv1d_depth"],
+        kernel_size=hparams["conv1d_size"],
+        strides=hparams["conv1d_stride"],
+        padding='SAME',
+        activation="relu"
+    )(rnn_imd)
+    for i in range(hparams["rnn_layers"]):
+        rnn_imd = layers.LSTM(
+            units=hparams["rnn_units"],
+            return_sequences=(i+1 < hparams["rnn_layers"]),
+        )(rnn_imd)
+
+    mlp_imd = []
+    for i in range(hparams["maxout_head"]):
+        mlp_imd.append(layers.Dense(units=hparams["maxout_units"])(mlp_input))
+
+    if hparams["maxout_head"] > 1:
+        mlp_imd = layers.Maximum()(mlp_imd)
+    elif hparams["maxout_head"] == 1:
+        mlp_imd = layers.Activation(activation="relu")(mlp_imd[0])
+    else:
+        mlp_imd = mlp_input
+
+    imd = layers.Concatenate(axis=-1)([rnn_imd, mlp_imd])
+    imd = layers.Dropout(rate=hparams["dropout"])(imd)
+
+    output_tf = layers.Dense(
+      units=1,
+      activation=tf.keras.activations.sigmoid
+    )(imd)
+
+    model = tf.keras.models.Model(inputs=[mlp_input, rnn_input], outputs=output_tf)
+
+    model.compile(
+      optimizer=build_optimizer(
+          optimizer_name=hparams["optimizer"],
+          learning_rate=hparams["learning_rate"],
+          decay=hparams["lr_decay"]
+      ),
+      loss=keras.losses.binary_crossentropy,
+      metrics=[
+        BinaryAccuracy(hparams["threshold"]),
+        BinaryMCC(hparams["threshold"]),
+        BinarySensitivity(hparams["threshold"]),
+        BinarySpecificity(hparams["threshold"])
+      ]
+    )
+
+    return model
