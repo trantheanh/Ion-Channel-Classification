4a8744173ccfa3140cfd38aec302db8e6df2915e
anhttmle <anhttmle@gmail.com>
2020-06-01 21:06:34 +0700

    update hparam

--- data_exp.py
+++ data_exp.py
@@ -134,7 +134,7 @@ def build_model() -> keras.models.Model:
 
     # imd = emb_imd
     imd = layers.Concatenate(axis=-1)([emb_imd, pssm_imd])
-    imd = layers.Dropout(rate=0.3)(imd)
+    # imd = layers.Dropout(rate=0.1)(imd)
     # imd = layers.Dense(units=512, activation="relu")(imd)
 
     output_tf = layers.Dense(
@@ -183,7 +183,7 @@ def train(train_ds, test_ds):
 
 def build_train_ds(emb_input, pssm_input, label):
     ds = tf.data.Dataset.from_tensor_slices(((emb_input, pssm_input), label))
-    ds = ds.shuffle(10000).batch(16)
+    ds = ds.shuffle(10000).batch(2)
     return ds
 
 
