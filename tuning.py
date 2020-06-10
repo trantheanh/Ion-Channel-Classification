71ae8f343d2d4e5ac8504b442c2433072856990c
unknown <anhttmle@gmail.com>
2020-06-10 15:08:17 +0700

    allow memory growth

--- tuning.py
+++ tuning.py
@@ -1,6 +1,15 @@
 import os
 import numpy as np
 from process.core import experiment
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
 
 batch_size = [1, 2, 4, 8, 16, 32]
 lr = (0.00001, 0.5)
